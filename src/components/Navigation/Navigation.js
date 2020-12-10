a8da2c8ba7c788bdc7882f8683e643fc7a29785f
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-10 15:59:45 +0900

    yarn add tachyons

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <nav sytle={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <p>Sign Out</p>
+    </nav>
+  );
+};
+
+export default Navigation;
