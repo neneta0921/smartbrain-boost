a2029087ef8ced6534584fc9c22a4da429053083
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-11 06:51:59 +0900

    add routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 
 import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
@@ -24,7 +26,7 @@ const app = new Clarifai.App({
 });
 
 const particlesOptions = {
-  polygon: {
+  particles: {
     number: {
       value: 30,
       density: {
@@ -42,15 +44,33 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
+      route: 'signin',
+      isSignedIn: false,
     };
   }
 
   calculateFaceLocation = (data) => {
+    // console.log(data);
+    // const regions = data.outputs[0].data.regions;
+    // const faceBox = regions.map((region) => {
+    // const detectedFace = region.region_info.bounding_box;
+    // const image = document.getElementById('inputimage');
+    // const width = Number(image.width);
+    // const height = Number(image.height);
+    // console.log(width, height);
+    // return {
+    // leftCol: detectedFace.left_col * width,
+    // topRow: detectedFace.top_row * height,
+    // rightCol: width - detectedFace.right_col * width,
+    // bottomRow: height - detectedFace.bottom_row * height,
+    // };
+    // });
+    // return faceBox;
+
     const detectedFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log(width, height);
     return {
       leftCol: detectedFace.left_col * width,
       topRow: detectedFace.top_row * height,
@@ -60,7 +80,6 @@ class App extends Component {
   };
 
   displayFaceBox = (box) => {
-    console.log(box);
     this.setState({ box: box });
   };
 
@@ -69,23 +88,46 @@ class App extends Component {
   };
 
   onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
+    this.setState({ imageUrl: this.state.input })
     app.models
       // this.state.imageUrl だとPOST:400エラーが起こるので注意
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
       .then((response) => this.displayFaceBox(this.calculateFaceLocation(response)))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+  };
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false })
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
+    this.setState({ route: route })
   };
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceDetection box={this.state.box} imageUrl={this.state.imageUrl} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
+          ? (
+            <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceDetection box={box} imageUrl={imageUrl} />
+            </div>
+            )
+          : ( route === 'signin'
+              ? <Signin onRouteChange={this.onRouteChange} />
+              : <Register onRouteChange={this.onRouteChange} />
+            )
+        }
       </div>
     );
   }
--- src/components/FaceDetection/FaceDetection.js
+++ src/components/FaceDetection/FaceDetection.js
@@ -2,6 +2,22 @@ import React from 'react';
 import './FaceDetection.css';
 
 const FaceDetection = ({ box, imageUrl }) => {
+  //  const createBoundingBox = () => {
+  //    const boundingBoxes = boxArray.map((box) => {
+  //      return (
+  //        <div
+  //          className="bounding-box"
+  //          style={{
+  //            top: box.topRow,
+  //            right: box.rightCol,
+  //            bottom: box.bottomRow,
+  //            left: box.leftCol,
+  //          }}></div>
+  //      );
+  //    });
+  //    return boundingBoxes;
+  //  };
+
   return (
     <div className="center ma">
       <div className="absolute mt2">
