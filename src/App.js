d4b40c89eaa634c8be222ac7be31674130f66529
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-10 17:22:11 +0900

    create components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import Navigation from './components/Navigation/Navigation';
+import Particles from 'react-particles-js';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+const particlesOptions = {
+  polygon: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
+
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
