f4de829d687f0b7c5627f125b9130b7d664c14b5
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-13 10:32:09 +0900

    API key move to backend

--- src/App.js
+++ src/App.js
@@ -11,20 +11,6 @@ import Rank from './components/Rank/Rank';
 import FaceDetection from './components/FaceDetection/FaceDetection';
 import './App.css';
 
-// HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-// A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-// for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-// If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-// is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
-// so you would change from:
-// .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-// to:
-// .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
-
-const app = new Clarifai.App({
-  apiKey: 'c2965edda9cd42cba2fd0383dda649ae',
-});
-
 const particlesOptions = {
   particles: {
     number: {
@@ -112,10 +98,15 @@ class App extends Component {
   };
 
   onPictureSubmit = () => {
-    this.setState({ imageUrl: this.state.input })
-    app.models
-      // this.state.imageUrl だとPOST:400エラーが起こるので注意
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    this.setState({ imageUrl: this.state.input });
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
       .then((response) => {
         if(response) {
           fetch('http://localhost:3000/image', {
